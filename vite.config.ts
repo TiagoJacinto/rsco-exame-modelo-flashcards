9d5613bfd44cee0e39cd5ac14012f643a16add0e
Tiago Jacinto <tiagojacintodev@gmail.com>
2026-01-15 16:33:22 +0000

    fix: define base path as repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/rsco-exame-modelo-flashcards/',
   plugins: [
     react({
       babel: {
